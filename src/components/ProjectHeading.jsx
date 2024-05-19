ce67a6e4a331d99553a77eb6f6beccb76b39b6f4
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2024-05-20 03:43:35 +0530

    added 3d objects on projects page

--- src/components/ProjectHeading.jsx
+++ src/components/ProjectHeading.jsx
@@ -5,7 +5,7 @@ export default function ProjectHeading() {
     <>
       <div className="relative">
   <div className="absolute inset-0 flex items-left justify-left pointer-events-none">
-    <h1 className="text-[#cce7f6] sm:text-left md:text-left w-1/2 lg:ml-4 px-8 text-3xl md:text-4xl lg:text-5xl py-12 title-font font-semibold hover:text-[#76BEF8] duration-300 mb-[-10] ">
+    <h1 className="text-[#cce7f6] sm:text-left md:text-left sm:2/3 md:w-1/2 lg:ml-4 px-8 text-3xl md:text-4xl lg:text-5xl py-12 title-font font-semibold hover:text-[#76BEF8] duration-300 mb-[-10] ">
       A few cool ideas that<br/> I've tried to bring to life.
     </h1>
   </div>
