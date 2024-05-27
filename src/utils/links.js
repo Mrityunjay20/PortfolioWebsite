661686a82e4bfb3ff17f713049fba71494c004f9
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2024-05-28 02:33:01 +0530

    routing issue fixed using vercel.json

--- src/utils/links.js
+++ src/utils/links.js
@@ -1,4 +1,4 @@
-export const resumeLink="https://drive.google.com/file/d/1DeD-sJT6NiNIhn9SyFh11AHt5VonYoM7/view?usp=sharing"
+export const resumeLink="https://drive.google.com/file/d/1V_S-aQcghQyI2AJe7o43-sid4JA_7a8h/view?usp=sharing"
 export const twitterLink="https://twitter.com/mjxshrivastava"
 export const linkedinLink="https://www.linkedin.com/in/mrityunjay-shrivastava/"
 export const githubLink="https://github.com/Mrityunjay20"
