4d26f8ac8774f80ca8fc82517c3bd1e764fcffa3
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2024-05-28 00:30:50 +0530

    resume updated

--- src/utils/links.js
+++ src/utils/links.js
@@ -1,4 +1,4 @@
-export const resumeLink="https://drive.google.com/file/d/1MEuexROPo0Z6GWMok1HwMeB8L61Z2Aa7/view?usp=sharing"
+export const resumeLink="https://drive.google.com/file/d/1DeD-sJT6NiNIhn9SyFh11AHt5VonYoM7/view?usp=sharing"
 export const twitterLink="https://twitter.com/mjxshrivastava"
 export const linkedinLink="https://www.linkedin.com/in/mrityunjay-shrivastava/"
 export const githubLink="https://github.com/Mrityunjay20"
